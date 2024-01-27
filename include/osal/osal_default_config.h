0a0848a9ff0b9e7b882b9d74878b00a9ddbf530d
Matt Chu <chumatthewp@gmail.com>
2024-01-27 14:29:24 -0800

    Removed std::string_view from OSAL configuration for now

--- include/osal/osal_default_config.h
+++ include/osal/osal_default_config.h
@@ -4,12 +4,7 @@
 namespace osal
 {
 
-#if __cplusplus >= 201703L
-    #include <string_view>
-    using string_view = std::string_view;
-#else
-    using string_view = const char *;
-#endif
+using string_view = const char *;
 
 #ifndef configOSAL_MAXIMUM_TASK_NAME_SIZE
     #define configOSAL_MAXIMUM_TASK_NAME_SIZE 64
@@ -23,13 +18,20 @@ namespace osal
     #define configOSAL_MAXIMUM_TASK_PRIORITY 0x7FFFFFFF
 #endif
 
-#if defined(WIN32) || defined(__linux__)
+#if defined(_WIN32) || defined(__linux__)
+    #define configOSAL_HAS_STD_THREAD_SUPPORT 1
+#else
+    #define configOSAL_HAS_STD_THREAD_SUPPORT 0
 #endif
 
 #ifndef configOSAL_SEMAPHORE_NATIVE_HANDLE
-    #if __cplusplus >= 202002L
-        #include <semaphore>
-        #define configOSAL_SEMAPHORE_NATIVE_HANDLE std::counting_semaphore::native_handle_type
+    #if configOSAL_HAS_STD_THREAD_SUPPORT
+        #if __cplusplus >= 202002L
+            #include <semaphore>
+            #define configOSAL_SEMAPHORE_NATIVE_HANDLE std::counting_semaphore::native_handle_type
+        #endif
+    #else
+        #error "configOSAL_SEMAPHORE_NATIVE_HANDLE is not defined for this platform!"
     #endif
 #endif
 
