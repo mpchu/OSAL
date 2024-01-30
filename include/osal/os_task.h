7cb4b4a7e22f3585e90e7cd48a9d83a6dfa46c4f
Matt Chu <chumatthewp@gmail.com>
2024-01-30 05:02:20 -0800

    Added Doxygen documentation for task public API class

--- include/osal/os_task.h
+++ include/osal/os_task.h
@@ -14,6 +14,11 @@ namespace osal
 namespace details
 {
 
+/**
+ * @brief Public API for an operating system task object.
+ * This class template declares operations and attributes common to all operating system tasks.
+ * @tparam task_impl OS-specific task implementation class
+ */
 template <class task_impl = configOSAL_TASK_IMPLEMENTATION>
 class os_task
 {
@@ -22,8 +27,14 @@ class os_task
     using callable_task_fn = std::function<void()>;
 
 public:
-    using native_handle_type = configOSAL_TASK_NATIVE_HANDLE;
+    using native_handle_type = configOSAL_TASK_NATIVE_HANDLE; /**< Underlying OS task handle implementation */
 
+    /**
+     * @brief Creates a new task in the operating system and executes it.
+     * @tparam Function Function to be executed by the task
+     * @param[in] attr Task attributes
+     * @param[in] func Function to execute
+     */
     template<class Function>
     explicit os_task(task_attributes attr, Function &&func)
         : _implementation(std::forward<task_attributes>(attr),
@@ -31,6 +42,14 @@ public:
     {
     }
 
+    /**
+     * @brief Creates a new task in the operating system and executes it.
+     * @tparam Function Function to be executed by the task
+     * @tparam Args Parameter pack of arguments to be passed to Function
+     * @param[in] attr Task attributes
+     * @param[in] func Function to execute
+     * @param[in] args Arguments to be passed to func
+     */
     template<class Function, class... Args>
     explicit os_task(task_attributes attr, Function &&func, Args &&... args)
         : _implementation(std::forward<task_attributes>(attr),
@@ -38,17 +57,36 @@ public:
     {
     }
 
+    /**
+     * @brief Destroys the task and cleans up any OS resources that were in use.
+     */
     ~os_task() = default;
 
     os_task(const os_task &rhs) = delete;
 
     os_task(os_task &&rhs) = default;
 
+    /**
+     * @brief Returns the underlying operating system task handle for this object.
+     * @return Native OS task handle
+     */
     native_handle_type native_handle() { return _implementation.native_handle(); }
 
+    /**
+     * @brief Returns the task attributes.
+     * @return Task attributes 
+     */
     task_attributes attributes() { return _implementation.attributes(); }
 
 private:
+    /**
+     * @brief Binds a parameter pack to a function to create a callable task definition object.
+     * @tparam Function Function to be executed by the task
+     * @tparam Args Parameter pack of arguments to be passed to Function
+     * @param[in] func Function to execute
+     * @param[in] args Arguments to be passed to func
+     * @return Callable task definition function
+     */
     template<class Function, class... Args>
     callable_task_fn create_callable_task(Function &&func, Args &&... args)
     {
--- include/osal/os_this_task.h
+++ include/osal/os_this_task.h
@@ -9,8 +9,15 @@ namespace osal
 namespace this_task
 {
 
+/**
+ * @brief Provides a hint to the operating system scheduler to yield execution time for this task to other tasks.
+ */
 void yield() noexcept;
 
+/**
+ * @brief Stops execution of this task for a set period of time.
+ * @param sleep_duration Time to sleep for in milliseconds
+ */
 void sleep_for(const std::chrono::milliseconds &sleep_duration);
 
 } // namespace this_task
