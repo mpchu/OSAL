c50c389a42d6deb008b163e03e2e4c166bc36014
Matt Chu <chumatthewp@gmail.com>
2024-02-04 13:54:40 -0800

    Adding a stack pointer member to osal::task_attributes for operating systems that allow static allocation of task stacks

--- include/osal/os_task_attributes.h
+++ include/osal/os_task_attributes.h
@@ -12,17 +12,24 @@ struct task_attributes
 {
 private:
     char _name[configOSAL_MAXIMUM_TASK_NAME_SIZE];
+    uint8_t *_stack_ptr;
     std::size_t _stack_size;
     int _priority;
 
 public:
-    task_attributes() : _stack_size(0), _priority(0) { std::memset(_name, '\0', sizeof(_name)); }
+    task_attributes() : _stack_ptr(nullptr), _stack_size(0), _priority(0) { std::memset(_name, '\0', sizeof(_name)); }
     ~task_attributes() = default;
 
     task_attributes &set_name(const char *name)
     {
         std::memset(_name, '\0', sizeof(_name));
         std::strncpy(_name, name, sizeof(_name) - 1);
+        return *this;
+    }
+
+    task_attributes &set_stack_pointer(uint8_t *stack_ptr)
+    {
+        _stack_ptr = stack_ptr;
         return *this;
     }
 
@@ -39,6 +46,7 @@ public:
     }
 
     const char *name()       { return _name; }
+    uint8_t *stack_pointer() { return _stack_ptr; }
     std::size_t stack_size() { return _stack_size; }
     int priority()           { return _priority; }
 };
