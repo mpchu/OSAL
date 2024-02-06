460a8e9c440cb0a52352d4ae972d9d1fbc5d15cd
Matt Chu <chumatthewp@gmail.com>
2024-02-05 19:06:22 -0800

    Cleaned up msg_queue_example.cpp

--- examples/msg_queue_example.cpp
+++ examples/msg_queue_example.cpp
@@ -10,9 +10,8 @@ using namespace std::chrono_literals;
 
 static constexpr uint32_t kMessageIdMultiplier = 100;
 
-template <class Duration>
 void producer_task(int id,
-                   const Duration delay,
+                   const std::chrono::milliseconds delay,
                    int burstAmount,
                    osal::message_queue &msgQ)
 {
@@ -46,7 +45,7 @@ void consumer_task(int id,
     }
 }
 
-void osal_msg_queue_example()
+void osal_queue_example()
 {
     constexpr std::size_t stack_size = 1024;
     constexpr int priority = 2; // For this example, task priorities range from 0 - 6
@@ -58,35 +57,35 @@ void osal_msg_queue_example()
             .set_name("ProducerTask 1")
             .set_stack_size(stack_size)
             .set_priority(priority),
-        producer_task<std::chrono::seconds>, 1, 1s, 3, std::ref(msgQ));
+        producer_task, 1, 1s, 3, std::ref(msgQ));
 
     osal::task p2(
         osal::task_attributes()
             .set_name("ProducerTask 2")
             .set_stack_size(stack_size)
             .set_priority(priority),
-        producer_task<std::chrono::milliseconds>, 2, 600ms, 3, std::ref(msgQ));
+        producer_task, 2, 600ms, 1, std::ref(msgQ));
 
     osal::task p3(
         osal::task_attributes()
             .set_name("ProducerTask 3")
             .set_stack_size(stack_size)
             .set_priority(priority),
-        producer_task<std::chrono::seconds>, 3, 3s, 3, std::ref(msgQ));
+        producer_task, 3, 3s, 4, std::ref(msgQ));
 
     osal::task p4(
         osal::task_attributes()
             .set_name("ProducerTask 4")
             .set_stack_size(stack_size)
             .set_priority(priority),
-        producer_task<std::chrono::milliseconds>, 4, 800ms, 3, std::ref(msgQ));
+        producer_task, 4, 800ms, 2, std::ref(msgQ));
 
     osal::task p5(
         osal::task_attributes()
             .set_name("ProducerTask 5")
             .set_stack_size(stack_size)
             .set_priority(priority),
-        producer_task<std::chrono::seconds>, 5, 2s, 3, std::ref(msgQ));
+        producer_task, 5, 2s, 3, std::ref(msgQ));
 
     osal::task c1(
         osal::task_attributes()
